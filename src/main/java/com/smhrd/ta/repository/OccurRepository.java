b9f6c4e2db8bc9463c9ab3a9d872ae4e0de8824a
smhrd <smhrd@192.168.219.58>
2025-06-24 09:38:21 +0900

    0624/ 송주찬

--- src/main/java/com/smhrd/ta/repository/OccurRepository.java
+++ src/main/java/com/smhrd/ta/repository/OccurRepository.java
@@ -9,7 +9,7 @@ import com.smhrd.ta.entity.OccurEntity;
 
 @Repository
 public interface OccurRepository extends JpaRepository<OccurEntity, String> {
-	List<OccurEntity> findWithFilters(String swLat, String neLat, String swLng, String neLng, List<String>regions, List<String>years, List<String>statuses);
+//	List<OccurEntity> findWithFilters(String swLat, String neLat, String swLng, String neLng, List<String>regions, List<String>years, List<String>statuses);
 	
 //	List<OccurEntity> findByLatitudeBetweenAndLongitudeBetween(String latStart, String latEnd, String lngStart,	String lngEnd);
 
